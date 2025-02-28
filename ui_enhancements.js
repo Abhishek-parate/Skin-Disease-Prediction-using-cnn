9f8aeb9d8405ea9031240fa613d7d518115f8f50
Abhishek-parate <abhishek.parate.mcm@ghrccst.raisoni.net>
2025-02-28 14:48:40 +0530

    Implement CNN architecture for skin disease classification

--- ui_enhancements.js
+++
@@ -1,9 +0,0 @@
-# Temporary file for commit history
-# Created: 2025-04-03 13:15:04
-
-// Frontend enhancements
-
-document.addEventListener('DOMContentLoaded', function() {
-    // TODO: Implement UI improvements
-    console.log('UI improvements loaded');
-});
