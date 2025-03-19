d81bce5428de9ceb377cbc51009fe60a425afc5f
Abhishek-parate <abhishek.parate.mcm@ghrccst.raisoni.net>
2025-03-19 15:06:00 +0530

    Fix class imbalance issues in the training dataset

---
+++ ui_enhancements.js
@@ -0,0 +1,9 @@
+# Temporary file for commit history
+# Created: 2025-04-03 13:16:04
+
+// Frontend enhancements
+
+document.addEventListener('DOMContentLoaded', function() {
+    // TODO: Implement UI improvements
+    console.log('UI improvements loaded');
+});
